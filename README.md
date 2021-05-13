c616e5e6bd4ca0c33c99dbe64306c1fb8c002545
Owen Osborn <owenosborn@gmail.com>
2021-05-13 01:05:15 -0400

    adding files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Serial_UDP
+
 receive packets from serial port, forward to a UDP socket
+
+compile:
+
+g++ -o main main.cpp Serial.cpp SLIPEncodedSerial.cpp UdpSocket.cpp Socket.cpp
+
+kill any process using the serial port:
+
+sudo fuser -k /dev/serial/by-id/usb-SparkFun_SFE_SAMD21_7600763950575035352E3120FF011F43-if00
+
+also appears ModemManager is grabbing the port when USB first attached, so disable this:
+
+sudo systemctl disable ModemManager.service
+sudo systemctl stop ModemManager.service
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "Serial.h"
+#include "SLIPEncodedSerial.h"
+#include "UdpSocket.h"
+
+Serial serial; 
+SLIPEncodedSerial slipdec;
+UdpSocket udpSock(5000);
+
+
+uint8_t buf[4096];
+
+int main() {
+    udpSock.setDestination(5001, "localhost");
+    
+    while (1) {
+        usleep(10000);
+
+        if (slipdec.recvMessage(serial)) {
+            udpSock.writeBuffer(slipdec.decodedBuf, slipdec.decodedLength);
+            //printf("got one \n");
+        }
+    }
+    //printf("error %d reading: %s", errno, strerror (errno));
+}
