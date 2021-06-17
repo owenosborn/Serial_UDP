5b501de29bdb2e3df6f0a1b02e27167d558d24f2
Owen Osborn <owenosborn@gmail.com>
2021-06-17 11:30:41 -0400

    add udp to serial

--- main.cpp
+++ main.cpp
@@ -4,20 +4,35 @@
 #include "UdpSocket.h"
 
 Serial serial; 
-SLIPEncodedSerial slipdec;
+SLIPEncodedSerial slip;
+
+// listen for incoming messages on 5000
 UdpSocket udpSock(5000);
 
 uint8_t buf[4096];
+int len;
 
 int main() {
-    udpSock.setDestination(5001, "localhost");
-    
-    while (1) {
-        usleep(10000);
 
-        if (slipdec.recvMessage(serial)) {
-            udpSock.writeBuffer(slipdec.decodedBuf, slipdec.decodedLength);
+    // send messages to 5001
+    udpSock.setDestination(5001, "localhost");
+
+    while (1) {
+
+        // poll interval
+        usleep(5000);
+
+        // serial in, UDP out
+        if (slip.recvMessage(serial)) {
             //printf("got one \n");
+            udpSock.writeBuffer(slip.decodedBuf, slip.decodedLength);
+        }
+    
+        // UDP in, serial out
+        len = udpSock.readBuffer(buf, 2048, 0);
+        if (len > 0) {
+            //printf("got one\n"); for (int i = 0; i < len; i++) { printf("%d", buf[i]); } printf("\n");
+            slip.sendMessage(buf, len, serial);
         }
     }
     //printf("error %d reading: %s", errno, strerror (errno));
