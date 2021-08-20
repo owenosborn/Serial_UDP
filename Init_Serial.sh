e166d1ca4c81c86e8af2f259e98b27d6271eb7b3
davidbongiorno <83716695+DBongiorno-CG@users.noreply.github.com>
2021-08-20 14:40:25 -0400

    Quick solution for obaining /dev name within Linux enviornment; removed .gch files

--- Init_Serial.sh
+++ Init_Serial.sh
@@ -4,13 +4,34 @@ echo -e "\n~~ Acquiring SAMD21 USB Device Name ~~ \n"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]] ## Prefix for Linux kernels
 then
-    export USB_DEV=$(ioreg -rbc IOUSBHostDevice -l -n "SFE SAMD21" | grep -o /dev/tty.usbmodem*) 
-     echo -e "The IO Dial In ID for the SAMD21 Device is:" $USB_DEV
+     #export USB_DEV=$(ioreg -rbc IOUSBHostDevice -l -n "SFE SAMD21" | grep -o /dev/tty.usbmodem*) 
+     for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && exit
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && exit
+
+
+        if [[ $(echo "/dev/$devname - $ID_SERIAL" | grep SAMD) ]]; then
+                echo "USB_DEV=/dev/$devname" >> temp.txt
+                exit
+        fi
+    )
+    done
+
+    source temp.txt
+    export USB_DEV
+    rm temp.txt
+     
+     
+     echo -e "The IO Dial-In ID for the SAMD21 Device is:" $USB_DEV
     
 elif [[ "$OSTYPE" == "darwin"* ]] ## Prefix for Mac OSX kernels
 then
     export USB_DEV=$(ioreg -rbc IOUSBHostDevice -l -n "SFE SAMD21" | grep -o /dev/cu.usbmodem*)
-    echo -e "The IO Callout ID for the SAMD21 Device is:" $USB_DEV
+    echo -e "The IO Call-Out ID for the SAMD21 Device is:" $USB_DEV
 
 else
     echo -e "Native OS not found!\n"
