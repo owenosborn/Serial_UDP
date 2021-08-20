42fc337957fe4b5686c891546b18eac5411f1623
davidbongiorno <83716695+DBongiorno-CG@users.noreply.github.com>
2021-08-20 12:15:02 -0400

    Created .gitignore, removed intermediate shell script, added OS detection for USB device name acquisition

--- Get_USB_Dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export USB_DEV=$(ioreg -rbc IOUSBHostDevice -l -n "SFE SAMD21" | grep -o /dev/cu.usbmodem*) 
-echo "The IO Callout ID for the SAMD21 Device is:" $USB_DEV
--- Init_Serial.sh
+++ Init_Serial.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 echo -e "\n~~ Acquiring SAMD21 USB Device Name ~~ \n"
-source Get_USB_Dev.sh
 
+if [[ "$OSTYPE" == "linux-gnu"* ]] ## Prefix for Linux kernels
+then
+    export USB_DEV=$(ioreg -rbc IOUSBHostDevice -l -n "SFE SAMD21" | grep -o /dev/tty.usbmodem*) 
+     echo -e "The IO Dial In ID for the SAMD21 Device is:" $USB_DEV
+    
+elif [[ "$OSTYPE" == "darwin"* ]] ## Prefix for Mac OSX kernels
+then
+    export USB_DEV=$(ioreg -rbc IOUSBHostDevice -l -n "SFE SAMD21" | grep -o /dev/cu.usbmodem*)
+    echo -e "The IO Callout ID for the SAMD21 Device is:" $USB_DEV
+
+else
+    echo -e "Native OS not found!\n"
+fi 
+
+
+
+echo -e "\n"
 echo -e "~~ Initializing Serial Ports ~~\n"
 ./main
 
